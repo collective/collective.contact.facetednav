4ea1a9c99f90bcc390e42efec04bb337c2c31bd1
Cédric Messiant <cedricmessiant@ecreall.com>
2014-08-08 11:17:50 +0200

    Fix conditional viewlet manager to really check that viewlets are available

--- src/collective/contact/facetednav/browser/actions/manager.py
+++ src/collective/contact/facetednav/browser/actions/manager.py
@@ -1,15 +1,50 @@
+"""Viewlet managers
+"""
 from zope.interface import implements
 from zope.viewlet.interfaces import IViewletManager
-from zope.viewlet.manager import ConditionalViewletManager
+from zope.viewlet.manager import WeightOrderedViewletManager
+from AccessControl.ZopeGuards import guarded_hasattr
 
 from collective.contact.facetednav.browser.view import ACTIONS_ENABLED_KEY
 
 
+def is_available(viewlet):
+    """Check if viewlet is available
+    """
+    if not guarded_hasattr(viewlet, 'render'):
+        return False
+
+    try:
+        if callable(viewlet.available):
+            available = viewlet.available()
+        else:
+            available = viewlet.available
+        return available
+    except AttributeError:
+        return True
+
+
+class ConditionalViewletManager(WeightOrderedViewletManager):
+    """Conditional weight ordered viewlet managers."""
+
+    def filter(self, viewlets):
+        """Sort out all viewlets which are explicit not available
+
+        ``viewlets`` is a list of tuples of the form (name, viewlet).
+        """
+        return [(name, viewlet) for name, viewlet in viewlets
+                if is_available(viewlet)]
+
+
 class IBatchActions(IViewletManager):
+    """Interface for batch actions viewlet manager
+    """
     pass
 
 
 class BatchActionsViewletManager(ConditionalViewletManager):
+    """Batch actions viewlet manager
+    """
     implements(IBatchActions)
 
 

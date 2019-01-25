d8cbd25c4e49e8567603cabd95d567252120c14f
Thomas Desvenain <thomas.desvenain@gmail.com>
2019-01-25 17:18:34 +0100

    [Plone 5] recook js

--- src/collective/contact/facetednav/upgrades/upgrades.py
+++ src/collective/contact/facetednav/upgrades/upgrades.py
@@ -1,3 +1,8 @@
+try:
+    from Products.CMFPlone.resources.browser.cook import cookWhenChangingSettings
+except ImportError:
+    cookWhenChangingSettings = None
+
 from ecreall.helpers.upgrade.interfaces import IUpgradeTool
 
 
@@ -5,11 +10,20 @@ def v2(context):
     tool = IUpgradeTool(context)
     tool.runProfile('collective.contact.facetednav.upgrades:v2')
 
+
 def v3(context):
     tool = IUpgradeTool(context)
     tool.runImportStep('collective.contact.facetednav', 'actions')
     tool.runImportStep('collective.contact.facetednav', 'jsregistry')
 
+
 def v4(context):
     tool = IUpgradeTool(context)
     tool.refreshResources()
+
+
+def v5(context):
+    tool = IUpgradeTool(context)
+    tool.refreshResources()
+    if cookWhenChangingSettings is not None:
+        cookWhenChangingSettings(context)
